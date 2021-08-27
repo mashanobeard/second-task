faccdd5452ad30b858fd3ffea4fa6eeea8197e28
mashanobeard <maryk016061@gmail.com>
2021-08-27 16:00:56 +0300

    routes

--- counter-counter/src/App.js
+++ counter-counter/src/App.js
@@ -1,17 +1,42 @@
 import './App.css';
-
+import { AppBar, Tabs, Tab } from '@material-ui/core';
+import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import About from './components/about';
 import CounterParent from './containers/CounterParent';
 
+
 function App() {
+const routes = ["/About","/Counters"];
+
   return (
-    <div className="app">
-      <h1 className="header">My counter</h1>
-       <CounterParent /> 
-      
-     
-      
-      
-    </div>
+      <div className="App">
+      <BrowserRouter>
+      <Route path = "/">
+      <AppBar position="static">
+        <Tabs>
+          <Tab 
+            label="About" 
+            value={routes[0]}
+            component = {Link}
+            to = {routes[0]} />
+          <Tab
+            label="Counters"
+            value={routes[1]}
+            component = {Link} 
+            to = {routes[1]}/>
+        </Tabs>
+      </AppBar>
+      </Route>
+
+      <Switch>
+      <Route path="/About" component ={About} />
+      <Route path="/Counters" component ={CounterParent} />
+
+
+      </Switch>
+      </BrowserRouter>
+      </div>
+  
   );
 }
 
---
+++ counter-counter/src/AppRoutes.js
@@ -0,0 +1,4 @@
+export const root = "/";
+export const checkCounter = "/counter";
+export const register = "/about";
+
